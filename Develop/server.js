6afb4d77ea6e59c7720b402ca4077c2169f14e36
Abuye Mamuye <abuyeye3@gmail.com>
2021-01-24 16:36:20 -0800

    create the app PORT

--- Develop/server.js
+++ Develop/server.js
@@ -2,3 +2,12 @@ const express = require("express");
 
 const app = express();
 const PORT = process.env.PORT || 8085;
+
+app.use(express.json());
+// app.use(express.());
+app.use(express.static("public"));
+
+require("./Routes/htmlRoutes")(app);
+require("./Routes/apiRoutes")(app);
+
+app.listen(PORT,() => console.log('Listening on PORT: ${PORT}'));
