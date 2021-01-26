b3b23600fb34f8a5469892b19f4096febb7fd40c
Abuye Mamuye <abuyeye3@gmail.com>
2021-01-25 17:37:20 -0800

    create the sever on port

--- Routes/apiRoutes.js
+++ Routes/apiRoutes.js
@@ -1,38 +1,41 @@
 
 const dataPath = require("../db/db.json");
 const fs = require('fs');
-
+// const path = require('path');
 let id = dataPath.length + 1;
 
 
 module.exports = function(app) {
-    app.get('api/notes', function(req, res) {
+app.get('api/notes', function(req, res) {
         res.json(dataPath);
-    });
+});
 
-    app.post('/api/notes', function(req, res) {
+app.post('/api/notes', function(req, res) {
         req.body.id = id++;
 
         dataPath.push(req.body);
 
-        fs.writeFile("./db/db.json", JSON.stringify(dataPath), function(err) {
+fs.writeFile("./db/db.json", JSON.stringify(dataPath), function(err) {
             if (err) throw err;
             console.log("Ready to read");
-        });
-    res.json(dataPath);
-    });
-    app.delete("/api/notes/id", function(req, res) {
+ });
+
+res.json(dataPath);
+
+ });
+ app.delete("/api/notes/:id", function(req, res) {
         let getId = req.params.id;
-        for (let i = 0; i<dataPath.length; i++); {
+        for (let i = 0; i<dataPath.length; i++) {
             console.log(dataPath[i].id, parseInt(getId));
             if (dataPath[i].id === parseInt(getId)){
-                dataPath.splice(i,1);
+                dataPath.splice(i, 1);
             }
         }
-        fs.writeFile("./db/db.json",JSON.stringify(dataPath),function(err) {
+ fs.writeFile("./db/db.json", JSON.stringify(dataPath), function(err) {
             if (err) throw err;
             console.log("Ready to read");
         })
+
       res.json(dataPath);
     })
 };
