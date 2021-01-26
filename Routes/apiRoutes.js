26c9c750d56d59956c48ac9a59642a2c90b4ad54
Abuye Mamuye <abuyeye3@gmail.com>
2021-01-25 21:52:41 -0800

    delete some file unwanted

--- Routes/apiRoutes.js
+++ Routes/apiRoutes.js
@@ -1,47 +1,92 @@
-
-const dataPath = require("../db/db.json");
-const fs = require('fs');
-let id = dataPath.length + 1;
+// const fs = require("fs");
+// const path = require("path");
 
 
-module.exports = function(app) {
-app.get('api/notes', function(req, res) {
-        res.json(dataPath);
-});
 
-app.post('/api/notes', function(req, res) {
-      req.body.id = id++;
 
-        dataPath.push(req.body);
+// app.get("/notes", (req, res) => {
+//     res.sendFile(path.join(__dirname, "/public/notes.html"));
+// });
 
-fs.writeFile("./db/db.json", JSON.stringify(dataPath), function(err) {
-            if (err) throw err;
+
+
+
+
+// app.get("/api/notes", (req, res) => {
+//     res.sendFile(path.join(__dirname, "/db/db.json"));
+// });
+
+
+
+// app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "/public/index.html"));
+// });
+
+
+// app.post("/api/notes", (req, res) => {
+//     let newNote = req.body;
+//     let noteList = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+//     let notelength = (noteList.length).toString();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // const dataPath = require("../db/db.json");
+// // const fs = require('fs');
+// // let id = dataPath.length + 1;
+
+
+// // module.exports = function(app) {
+// // app.get('api/notes', function(req, res) {
+// //         res.json(dataPath);
+// // });
+
+// // app.post('/api/notes', function(req, res) {
+// //       req.body.id = id++;
+
+// //         dataPath.push(req.body);
+
+// // fs.writeFile("./db/db.json", JSON.stringify(dataPath), function(err) {
+// //             if (err) throw err;
            
-            console.log("Ready to read");
- });
+// //             console.log("Ready to read");
+// //  });
 
-res.json(dataPath);
+// // res.json(dataPath);
 
- });
- app.delete("/api/notes/:id", function(req, res) {
+// //  });
+// //  app.delete("/api/notes/:id", function(req, res) {
 
-        let getId = req.params.id;
+// //         let getId = req.params.id;
         
-        for (let i = 0; i < dataPath.length; i++) {
+// //         for (let i = 0; i < dataPath.length; i++) {
 
-            console.log(dataPath[i].id, parseInt(getId));
+// //             console.log(dataPath[i].id, parseInt(getId));
 
-            if (dataPath[i].id === parseInt(getId)){
+// //             if (dataPath[i].id === parseInt(getId)){
 
-                dataPath.splice(i, 1);
-            }
-        }
-        fs.writeFile("./db/db.json", JSON.stringify(dataPath), function(err) {
-            if (err) throw err;
+// //                 dataPath.splice(i, 1);
+// //             }
+// //         }
+// //         fs.writeFile("./db/db.json", JSON.stringify(dataPath), function(err) {
+// //             if (err) throw err;
           
-            console.log("Ready to read");
-        })
+// //             console.log("Ready to read");
+// //         })
 
-      res.json(dataPath);
-    })
-};
+// //       res.json(dataPath);
+// //     })
+// // };
--- server.js
+++ server.js
@@ -1,15 +1,56 @@
+const fs = require("fs");
 const express = require("express");
-const fs = require('fs');
-const path = require('path');
+const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 3002;
 
+const PORT = process.env.PORT || 8000;
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(express.static("public"));
 
-require("./routes/htmlRoutes")(app);
-require("./routes/apiRoutes")(app);
 
-app.listen(PORT,() => console.log('Listening on PORT: ${PORT}'));
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+//route to read the `db.json` 
+app.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+
+app.post("/api/notes", (req, res) => {
+    let newNote = req.body;
+    let noteList = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let notelength = (noteList.length).toString();
+
+    newNote.id = notelength;   
+    noteList.push(newNote);
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(noteList));
+    res.json(noteList);
+})
+
+//delete note 
+app.delete("/api/notes/:id", (req, res) => {
+    let noteList = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let noteId = (req.params.id).toString();
+
+    
+    noteList = noteList.filter(selected =>{
+        return selected.id != noteId;
+    })
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(noteList));
+    res.json(noteList);
+});
+
+
+//listen tot he port when deployed
+app.listen(PORT, () => console.log("Server listening on port " + PORT));
